614491f3f4a86f5177e09fdbf31cd5a2f7d075e5
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-06-09 21:15:51 +0300

    dark light changed

--- src/js/header.js
+++ src/js/header.js
@@ -21,3 +21,27 @@ const toggleBtn = document.getElementById("menu-toggle");
 toggleBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+const themeBtn = document.getElementById("theme-toggle");
+const themeIcon = themeBtn.querySelector("i");
+
+// Tema durumu yerel depodan alınır
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "light") {
+  document.body.classList.add("light-mode");
+  themeIcon.classList.replace("fa-moon", "fa-sun");
+}
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+
+  const isLight = document.body.classList.contains("light-mode");
+
+  
+  if (isLight) {
+    themeIcon.classList.replace("fa-moon", "fa-sun");
+    localStorage.setItem("theme", "light");
+  } else {
+    themeIcon.classList.replace("fa-sun", "fa-moon");
+    localStorage.setItem("theme", "dark");
+  }
+});
